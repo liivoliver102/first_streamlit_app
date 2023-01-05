4720fdaefca11a1e6d877e0e9b4b921ca062ac6e
liivoliver102 <121933827+liivoliver102@users.noreply.github.com>
2023-01-05 10:10:55 +0200

    Update streamlit_app.py
    changed the fruity vice to kiwi

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,8 +25,7 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kivi")
 
 # changes the json output so that aligns columns and stuff an takes comas, away 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
